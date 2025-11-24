297845151fba8a0edd51e2cbf352dfe3da5b9b85
Thanu <125559633+ThanujaJeewanthi@users.noreply.github.com>
2025-11-24 17:55:09 +0530

    Implement AWS Glue job start in Lambda function

---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+import boto3
+
+def lambda_handler(event, context):
+    glue = boto3.client('glue')
+    job_name = 's3-glue-s3'
+    try:
+        response = glue.start_job_run(JobName=job_name)
+        return {
+            'statusCode': 200,
+            'body': f"Glue job '{job_name}' with JobRunId: {response['JobRunId']} started successfully"
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f"Error starting Glue job '{job_name}': {str(e)}"
+        }
